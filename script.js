e44d248cd717dd0b05e54fe0d2dd5ba18c995def
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-21 08:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height)
+	{
+		this.width = width
+		this.height = height
+	}
+	get getArea()
+	{
+		return (${this.height}*${this.width})
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(n)
+	{
+		this.n = n
+	}
+	get getPerimeter()
+	{
+		return ${this.n}*4
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
